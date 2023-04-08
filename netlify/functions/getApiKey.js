14f34c6ce3132e80fe76e3733acbc9dc823286ba
AlonCohen96 <aloncohen36@gmail.com>
2023-04-08 17:16:04 +0200

    update

--- netlify/functions/getApiKey.js
+++ netlify/functions/getApiKey.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            apiKey: process.env.REACT_APP_API_KEY
+            apiKey: process.env.API_KEY
         }),
     }
 }
